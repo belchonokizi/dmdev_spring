111a4ca339cd42ed437719e164104eb3aa2a6b81
belchonokizi <belchonokizi@yandex.ru>
2023-04-13 12:45:14 +0300

    Add Spring Boot

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -1,14 +1,11 @@
 package com.dmdev.spring;
 
-import com.dmdev.spring.config.ApplicationConfiguration;
-import com.dmdev.spring.service.CompanyService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ApplicationRunner {
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
-            var service = context.getBean("companyService", CompanyService.class);
-            service.findById(1);
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
