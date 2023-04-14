48dd9f8da89a25cf778db7e279d444c76bd7cd54
belchonokizi <belchonokizi@yandex.ru>
2023-04-14 12:39:39 +0300

    Add IT CompanyServiceIT

---
+++ src/test/java/com/dmdev/spring/integration/annotation/IT.java
@@ -0,0 +1,17 @@
+package com.dmdev.spring.integration.annotation;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//создаем аннотацию, чтобы не дублировать все аннотации в IT
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest
+@ActiveProfiles("test")
+public @interface IT {
+}
---
+++ src/test/java/com/dmdev/spring/integration/service/CompanyServiceIT.java
@@ -0,0 +1,32 @@
+package com.dmdev.spring.integration.service;
+
+import com.dmdev.spring.config.DatabaseProperties;
+import com.dmdev.spring.dto.CompanyReadDto;
+import com.dmdev.spring.integration.annotation.IT;
+import com.dmdev.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@IT
+@RequiredArgsConstructor
+public class CompanyServiceIT {
+    private static final Integer COMPANY_ID = 1;
+
+    private final CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
+
+    @Test
+    void findById() {
+        Optional<CompanyReadDto> actualResult = companyService.findById(COMPANY_ID);
+
+        assertTrue(actualResult.isPresent());
+
+        CompanyReadDto expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
+    }
+}
