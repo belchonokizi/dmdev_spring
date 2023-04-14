c3458c5eaaffb4f65181198fc45e281c64ad351f
belchonokizi <belchonokizi@yandex.ru>
2023-04-14 10:42:14 +0300

    Add test starter, CompanyServiceTest

---
+++ src/test/java/com/dmdev/spring/service/CompanyServiceTest.java
@@ -0,0 +1,53 @@
+package com.dmdev.spring.service;
+
+import com.dmdev.spring.database.entity.Company;
+import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.spring.dto.CompanyReadDto;
+import com.dmdev.spring.listener.entity.EntityEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
+class CompanyServiceTest {
+    private static final Integer COMPANY_ID = 1;
+
+    @Mock
+    private UserService userService;
+    @Mock
+    private CrudRepository<Integer, Company> repository;
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
+    @InjectMocks
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+        Mockito.when(repository.findById(COMPANY_ID))
+                .thenReturn(Optional.of(new Company(COMPANY_ID)));
+
+        Optional<CompanyReadDto> actualResult = companyService.findById(COMPANY_ID);
+
+        assertTrue(actualResult.isPresent());
+
+        CompanyReadDto expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
+
+        //проверяем, что моки были вызваны
+        verify(eventPublisher).publishEvent(any(EntityEvent.class));
+        //что больше не было вызовов моков
+        verifyNoMoreInteractions(eventPublisher, userService);
+    }
+}
