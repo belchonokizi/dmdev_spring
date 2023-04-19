77f63fd587b7fca46385670b24d494a85e7a7176
belchonokizi <belchonokizi@yandex.ru>
2023-04-19 18:16:13 +0300

    Using Modifying for DML operation

--- src/main/java/com/dmdev/spring/database/repository/UserRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.dmdev.spring.database.repository;
 
+import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.database.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -11,4 +13,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("select u from User u " +
             "where u.firstname like %:firstname% and u.lastname like %:lastname%")
     List<User> findAllBy(String firstname, String lastname);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update User u " +
+            "set u.role = :role " +
+            "where u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
 }
--- src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.dmdev.spring.integration.database.repository;
 
+import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.database.entity.User;
 import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.integration.annotation.IT;
@@ -9,13 +10,26 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 @IT
 @RequiredArgsConstructor
 class UserRepositoryTest {
 
     private final UserRepository userRepository;
+
+    @Test
+    void checkUpdate() {
+        User ivan = userRepository.getById(1L);
+        assertSame(Role.ADMIN, ivan.getRole());
+
+        int resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        assertEquals(2, resultCount);
+
+        User theSameIvan = userRepository.getById(1L);
+        assertSame(Role.USER, theSameIvan.getRole());
+    }
 
     @Test
     void checkQueries() {
