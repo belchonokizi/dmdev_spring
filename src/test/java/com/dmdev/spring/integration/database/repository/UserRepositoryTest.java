5cbaec9ddb818c550a18305a5805f4f9f1a2ec08
belchonokizi <belchonokizi@yandex.ru>
2023-04-24 16:18:57 +0300

    Using Sort and Pageable in queries

--- src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
@@ -6,18 +6,45 @@ import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 @IT
 @RequiredArgsConstructor
 class UserRepositoryTest {
 
     private final UserRepository userRepository;
+
+    @Test
+    void checkPageable() {
+        PageRequest pageRequest = PageRequest.of(1, 2, Sort.by("id"));
+        List<User> result = userRepository.findAllBy(pageRequest);
+        assertThat(result).hasSize(2);
+    }
+
+    @Test
+    void checkSort() {
+        Sort.TypedSort<User> sortBy = Sort.sort(User.class);
+        Sort sort = sortBy.by(User::getFirstname)
+                .and(sortBy.by(User::getLastname));
+
+        List<User> allUsers = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sort);
+        assertThat(allUsers).hasSize(3);
+    }
+
+    @Test
+    void checkFirstTop() {
+        Optional<User> topUser = userRepository.findTopByOrderByIdDesc();
+        assertTrue(topUser.isPresent());
+        topUser.ifPresent((user -> assertEquals(5L, user.getId())));
+    }
 
     @Test
     void checkUpdate() {
