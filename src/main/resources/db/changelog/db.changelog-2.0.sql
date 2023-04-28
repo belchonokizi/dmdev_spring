5c4a4e864deb70cefea13f073cd62b251f0ef6c3
belchonokizi <belchonokizi@yandex.ru>
2023-04-28 19:07:05 +0300

    Using Liquibase

--- src/main/resources/db/changelog/db.changelog-2.0.sql
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+
+--changeset ibalobanova:1
 ALTER TABLE users
 ADD COLUMN created_at TIMESTAMP;
 
@@ -9,5 +12,3 @@ ADD COLUMN created_by VARCHAR(32);
 
 ALTER TABLE users
 ADD COLUMN modified_by VARCHAR(32);
-
-commit
