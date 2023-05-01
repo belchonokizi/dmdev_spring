58dc7b72bf759e4dd36f3202831a157e95269f34
belchonokizi <belchonokizi@yandex.ru>
2023-05-01 17:43:08 +0300

    Using Model in request

--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.dmdev.spring.http.controller;
 
+import com.dmdev.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,21 +9,31 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/api/v1")
+//указываем сессионные ключи
+@SessionAttributes({"user"})
 public class GreetingController {
 
+    @GetMapping("/hello")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
+        modelAndView.setViewName("greeting/hello");
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+        return modelAndView;
+    }
+
+    @GetMapping("/bye")
+    public ModelAndView bye(@SessionAttribute("user")UserReadDto user) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("greeting/bye");
+        return modelAndView;
+    }
+
     @GetMapping("/hello/{id}")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
                               @RequestParam Integer age,
                               @RequestHeader String accept,
                               @CookieValue("JSESSIONID") String jsessionId,
                               @PathVariable Integer id) {
         modelAndView.setViewName("greeting/hello");
-        return modelAndView;
-    }
-
-    @GetMapping("/bye")
-    public ModelAndView bye(ModelAndView modelAndView) {
-        modelAndView.setViewName("greeting/bye");
         return modelAndView;
     }
 }
