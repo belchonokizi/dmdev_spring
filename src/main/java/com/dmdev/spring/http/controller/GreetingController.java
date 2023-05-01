39a33da96622fc8900e38f23a9d2cfeaefb41eba
belchonokizi <belchonokizi@yandex.ru>
2023-05-01 18:04:50 +0300

    Using @ModelAttribute

--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -1,11 +1,15 @@
 package com.dmdev.spring.http.controller;
 
+import com.dmdev.spring.database.entity.Role;
 import com.dmdev.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1")
@@ -13,18 +17,23 @@ import javax.servlet.http.HttpServletRequest;
 @SessionAttributes({"user"})
 public class GreetingController {
 
+    //данный атрибут будет добавляться в модель при любом запросе
+    @ModelAttribute("roles")
+    public List<Role> roles() {
+        return Arrays.asList(Role.values());
+    }
+
     @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request) {
-        modelAndView.setViewName("greeting/hello");
-        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
-        return modelAndView;
+    public String hello(Model model,
+                        HttpServletRequest request,
+                        @ModelAttribute("userReadDto") UserReadDto userReadDto) {
+        model.addAttribute("user", new UserReadDto(1L, "Ivan"));
+        return "greeting/hello";
     }
 
     @GetMapping("/bye")
-    public ModelAndView bye(@SessionAttribute("user")UserReadDto user) {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("greeting/bye");
-        return modelAndView;
+    public String bye(@SessionAttribute("user") UserReadDto user) {
+        return "greeting/bye";
     }
 
     @GetMapping("/hello/{id}")
---
+++ src/main/webapp/WEB-INF/jsp/user/login.jsp
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+<form action="/login" method="post">
+    <label for="username"> Username:
+        <input type="text" name="username">
+    </label><br>
+    <label for="password"> Password:
+        <input type="text" name="password">
+    </label><br>
+    <button type="submit">Login</button>
+</form>
+</body>
+</html>
